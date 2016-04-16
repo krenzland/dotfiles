1ff7f2c034b1282d37324563bb6b35b94a74448f
Lukas Krenz <lukas@krenz.land>
2016-04-16 17:20:35 +0200

    Add installer.

---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+
+install-dotfiles() {
+    printf "Installing dotfiles!\n"
+}
+
+install-dependencies() {
+    printf "Feature install-dependencies currently not supported!.\n"
+}
+
+show-help() {
+    cat <<EOF
+Welcome to my configuration installer.
+Usage:
+-d | --dependencies    Install programs that I use often.
+-c | --config          Install my dotfiles.
+-h | --help            Show this document.
+EOF
+}
+
+main() {
+    if [ "$1" = true ]; then
+        install-dependencies
+    fi
+    if [ "$2" = true ]; then
+        install-dotfiles
+    fi
+
+}
+
+installDep=false
+installDot=false
+
+if [[ $# -eq 0 ]]; then
+    show-help
+fi
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -d|--dependencies)
+    installDep=true
+    ;;
+    -c|--config)
+    installDot=true
+    ;;
+    -h|--help)
+    show-help
+    exit 1
+    ;;
+    *)
+    printf "Unknown argument $1\n"
+    show-help
+    exit 1
+    ;;
+esac
+shift
+done
+
+main $installDep $installDot
