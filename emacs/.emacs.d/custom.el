01ecc10dcf9b47431433af2d146bf7d64fde9758
Lukas Krenz <lukas@krenz.land>
2016-12-27 20:03:55 +0100

    Add custom.el.

---
+++ emacs/.emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil-magit evil-escape which-key web-mode use-package solarized-theme smartparens smart-mode-line rustfmt racer projectile notmuch magit ivy-bibtex intero general flycheck-rust evil counsel company-quickhelp company-anaconda cargo avy auctex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
