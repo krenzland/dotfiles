eece629c97f2d18e76499400a4d7bfe2bd6fa9c4
Lukas Krenz <lukas@krenz.land>
2017-01-03 15:03:44 +0100

    Add ranger.

--- emacs/.emacs.d/src/init-util.el
+++ emacs/.emacs.d/src/init-util.el
@@ -1,12 +1,19 @@
 (use-package smartparens-config
   :ensure smartparens
+  :diminish (smartparens-mode . "")
   :init
   (smartparens-global-mode))
 
 (use-package avy
   :ensure t
   :commands (avy-goto-word-1))
+
 (winner-mode t)
+
+(use-package ranger
+  :ensure t
+  :config
+  (ranger-override-dired-mode t))
 
 (defun init-open-config ()
   "Opens the config file."
