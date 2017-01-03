27a6bdd47145f238490ecf43ee6be5155bc33cd9
Lukas Krenz <lukas@krenz.land>
2017-01-03 15:04:37 +0100

    Update ivy-config.

--- emacs/.emacs.d/src/init-ivy.el
+++ emacs/.emacs.d/src/init-ivy.el
@@ -5,26 +5,32 @@
   (:map ivy-mode-map
    ("C-#" . ivy-avy))
   :config
-  (progn
   (ivy-mode 1)
-  ;; add ‘recentf-mode’ and bookmarks to ‘ivy-switch-buffer’.
   (setq ivy-use-virtual-buffers t)
-  ;; number of result lines to display
-  (setq ivy-height 10)
-  ;; does not count candidates
-  (setq ivy-count-format "")
+  (setq ivy-count-format "(%d/%d)")
   ;; no regexp by default
-  (setq ivy-initial-inputs-alist nil)
+  ;; (setq ivy-initial-inputs-alist nil)
   ;; configure regexp engine.
   (setq ivy-re-builders-alist
         ;; allow input not in order
-        '((t   . ivy--regex-ignore-order)))))
+        '((t   . ivy--regex-ignore-order))))
+
+(use-package ivy-hydra
+  :ensure t)
 
 (use-package counsel
   :ensure t
   :bind
   (("M-x" . counsel-M-x)
   ("C-s" . counsel-grep-or-swiper)
-  ("C-x C-f" . counsel-find-file)))
+  ("C-x C-f" . counsel-find-file)
+  ("C-c g" . counsel-git)
+  ("C-c k" . counsel-git-grep)
+  ("C-x l" . counsel-locate)
+  
+  ))
+
+(use-package smex
+  :ensure t)
 
 (provide 'init-ivy)
