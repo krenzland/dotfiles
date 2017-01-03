d722db6e81590fcae053cfe33fc083c304ebd15b
Lukas Krenz <lukas@krenz.land>
2017-01-03 15:03:52 +0100

    Fix modes for web-mode.

--- emacs/.emacs.d/src/init-web.el
+++ emacs/.emacs.d/src/init-web.el
@@ -1,15 +1,13 @@
 (use-package web-mode
   :ensure t
-  :config
-  (progn 
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
-)
-
+  :mode
+  ("\\.html?\\'" . web-mode)
+  ("\\.phtml\\'" . web-mode)
+  ("\\.tpl\\.php\\'" . web-mode)
+  ("\\.[agj]sp\\'" . web-mode)
+  ("\\.as[cp]x\\'" . web-mode)
+  ("\\.erb\\'" . web-mode)
+  ("\\.mustache\\'" . web-mode)
+  ("\\.djhtml\\'" . web-mode))
+ 
 (provide 'init-web)
