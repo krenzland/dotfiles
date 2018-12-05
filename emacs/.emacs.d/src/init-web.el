88b1f4df432eb1b92856582288475f5499ee5530
Lukas Krenz <lukas@krenz.land>
2018-12-05 17:58:43 +0100

    Add emmet mode to web.

--- emacs/.emacs.d/src/init-web.el
+++ emacs/.emacs.d/src/init-web.el
@@ -9,6 +9,16 @@
   ("\\.erb\\'" . web-mode)
   ("\\.mustache\\'" . web-mode)
   ("\\.djhtml\\'" . web-mode))
+
+(use-package emmet-mode
+  :ensure t
+  :commands (emmet-mode
+             emmet-next-edit-point
+             emmet-prev-edit-point)
+  :config
+  ;; Auto-start on any markup modes
+  (add-hook 'sgml-mode-hook 'emmet-mode)
+  (add-hook 'web-mode-hook 'emmet-mode))
  
 (use-package restclient
   :ensure t
